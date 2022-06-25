22668702ef5f63e2364a5096d06a1b35e80e44ec
Drobotiuk Andrii <adrobotyuk@gmail.com>
2022-06-25 13:56:00 +0600

    first middleware

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const sequelize = require('./db')
 const models = require('./models/models')
 const cors = require('cors')
 const router =  require('./routes/index')
+const errorHandler = require('./middleware/ErrorHandlerMiddleware')
 
 
 
@@ -14,6 +15,9 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use('/api', router)
+
+
+app.use(errorHandler)
 
 
 const start = async () =>{
