98a2316f679e92df4bcaf5e16979455a863cae5f
Drobotiuk Andrii <adrobotyuk@gmail.com>
2022-06-27 12:18:07 +0600

    registration, auth & role added

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -15,11 +15,11 @@ class UserController {
     async registration(req, res, next) {
         const {email, password, role} = req.body
         if (!email || !password) {
-            return next(ApiError.badRequest('Некорректный email или password'))
+            return next(ApiError.badRequest('Incorrect email or password'))
         }
         const candidate = await User.findOne({where: {email}})
         if (candidate) {
-            return next(ApiError.badRequest('Пользователь с таким email уже существует'))
+            return next(ApiError.badRequest('User with this email already registered'))
         }
         const hashPassword = await bcrypt.hash(password, 5)
         const user = await User.create({email, role, password: hashPassword})
@@ -29,17 +29,23 @@ class UserController {
     }
 
 
-    async login(req, res) {
-
+    async login(req, res, next) {
+        const {email, password} = req.body
+        const user = await User.findOne({where: {email}})
+        if (!user) {
+            return next(ApiError.internal('Пользователь не найден'))
+        }
+        let comparePassword = bcrypt.compareSync(password, user.password)
+        if (!comparePassword) {
+            return next(ApiError.internal('Указан неверный пароль'))
+        }
+        const token = generateJwt(user.id, user.email, user.role)
+        return res.json({token})
     }
 
     async check(req, res, next) {
-        const {id} = req.query
-        if (!id) {
-            return next(ApiError.badRequest('Не задан ID'))
-        }
-        res.json(id)
-
+        const token = generateJwt(req.user.id, req.user.email, req.user.role)
+        return res.json({token})
     }
 }
 
